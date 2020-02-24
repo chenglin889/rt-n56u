e5224d9298f9240c8a3ab76063ec3ff51f0f49a8
chenglin889 <60931673+chenglin889@users.noreply.github.com>
2020-02-24 09:23:05 -0600

    Update koolproxy.sh

--- trunk/user/koolproxy/koolproxy.sh
+++ trunk/user/koolproxy/koolproxy.sh
@@ -1115,9 +1115,9 @@ if [ ! -f "$koolproxy_rules_list" ] || [ ! -s "$koolproxy_rules_list" ] ; then
 #（可选项：前面添加#停用规则,删除前面的#可生效）
 # 开关 0表示关闭 1表示开启
 # 开关|规则名字|规则网址|规则备注名字
-1|koolproxy.txt|https://kprule.com/koolproxy.txt|
-1|daily.txt|https://kprule.com/daily.txt|
-1|kp.dat|https://kprule.com/kp.dat|
+1|koolproxy.txt|https://houzi-.coding.net/p/my_dream/d/my_dream/git/raw/master/koolproxy.txt|
+1|daily.txt|https://houzi-.coding.net/p/my_dream/d/my_dream/git/raw/master/daily.txt|
+1|kp.dat|https://houzi-.coding.net/p/my_dream/d/my_dream/git/raw/master/kp.dat|
 1|user.txt||
 
 EEE
